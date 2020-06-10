c12a30791d63c9d0967b44c330829c6451725a39
Celia Gomez <celia.gomez@unumotors.com>
2020-06-10 16:42:10 +0200

    style: update global styles

---
+++ src/pages/404.js
@@ -0,0 +1,8 @@
+import React from "react"
+import "../assets/style/main.scss"
+
+export default function ErrorPage() {
+  return (
+    <div className="error-page">There was an error loading this page. Try again.</div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,19 @@ import About from "../components/about"
 import "../assets/style/main.scss"
 
 export default function Home() {
+<<<<<<< HEAD
   return pug`
     .div
       .grid.main
         About
   `
+=======
+  return (
+    <div>
+      <div className="grid main">
+        <About />
+      </div>
+    </div>
+  )
+>>>>>>> style: update global styles
 }
