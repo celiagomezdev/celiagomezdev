78ffba13fd1af69f60b34827c35e8183dba73b84
Celia Gomez <celia.gomez@unumotors.com>
2020-06-30 21:08:56 +0200

    fix: issue with internal anchor links

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -10,10 +10,10 @@ export default function NavBar() {
       </h3>
       <ul className={styles.menu}>
         <li><Link to="/">Home</Link></li>
-        <li><Link to="#about">About</Link></li>
-        <li><Link to="#projects">Projects</Link></li>
-        <li><Link to="#skills">Skills</Link></li>
-        <li><Link to="#inspiration">Inspiration</Link></li>
+        <li><Link to="/#about">About</Link></li>
+        <li><Link to="/#projects">Projects</Link></li>
+        <li><Link to="/#skills">Skills</Link></li>
+        <li><Link to="/#inspiration">Inspiration</Link></li>
       </ul>
     </header>
   )
