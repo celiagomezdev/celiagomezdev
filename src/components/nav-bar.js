5717020578ba3d440848163c5dae22f6503dd314
Celia Gomez <celia.gomez@unumotors.com>
2020-06-30 18:23:49 +0200

    feat: add temporary sections

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -11,6 +11,9 @@ export default function NavBar() {
       <ul className={styles.menu}>
         <li><Link to="/">Home</Link></li>
         <li><Link to="#about">About</Link></li>
+        <li><Link to="#projects">Projects</Link></li>
+        <li><Link to="#skills">Skills</Link></li>
+        <li><Link to="#inspiration">Inspiration</Link></li>
       </ul>
     </header>
   )
