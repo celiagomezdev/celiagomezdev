9bde7a68d19c4b3883da1ff3da2b013a9c6c79ce
Celia Gomez <celia.gomez@unumotors.com>
2020-08-03 17:50:54 +0200

    feat: add middle section

---
+++ src/components/middle-section.js
@@ -0,0 +1,10 @@
+import React from "react"
+import styles from "./middle-section.module.scss"
+
+export default function MiddleSection() {
+  return (
+    <section id={styles.middleSection} className="content">
+      <div className="grid"></div>
+    </section>
+  )
+}
