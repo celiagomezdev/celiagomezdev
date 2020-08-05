6d616628dd37e0e8f05cd289c918f4a422035b7a
Celia Gomez <celia.gomez@unumotors.com>
2020-08-05 09:30:11 +0200

    feat: add middle section wrappers

--- src/components/middle-section.js
+++ src/components/middle-section.js
@@ -3,8 +3,11 @@ import styles from "./middle-section.module.scss"
 
 export default function MiddleSection() {
   return (
-    <section id={styles.middleSection} className="content">
-      <div className="grid"></div>
+    <section id={styles.middleSection}>
+      <div className="content grid">
+        <div className={styles.carouselWrapper}></div>
+        <div className={styles.textWrapper}></div>
+      </div>
     </section>
   )
 }
