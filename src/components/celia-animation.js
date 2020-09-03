0ddd514c540b3fcc3103f837df908b07ae6e6d3d
Celia Gomez <celia.gomez@unumotors.com>
2020-09-03 17:42:05 +0200

    feat: add celia animation transition

--- src/components/celia-animation.js
+++ src/components/celia-animation.js
@@ -1,11 +1,27 @@
 import React from "react"
 import styles from "./celia-animation.module.scss"
-import celiaFrames from "../static/img/celia-frames.png"
+import celiaFramesImage from "../static/img/celia-frames.png"
 
 export default function CeliaAnimation() {
+  const celiaAnimation = React.createRef()
+  const celiaFrames = React.createRef()
+
+  window.onscroll = () => moveCeliaDownAndUp()
+
+  const moveCeliaDownAndUp = () => {
+    const celiaStyle = celiaAnimation.current.style
+    if (window.scrollY<=290) celiaStyle.setProperty('transform', `translateY(${window.scrollY/8}rem)`)
+    else if (window.scrollY === 0) celiaStyle.setProperty('transform', `translateY(0rem)`)
+    else celiaStyle.setProperty('transform', `translateY(36rem)`)
+  }
+
   return(
-    <div className={styles.celiaContainer}>
-      <img src={celiaFrames} alt="Celia Animation"></img>
+    <div id={styles.celiaAnimation} className="content grid" ref={celiaAnimation}>
+      <div className={styles.celiaContainer}>
+        <div className={styles.celia} ref={celiaFrames}>
+          <img src={celiaFramesImage} alt="Celia Animation"></img>
+        </div>
+      </div>
     </div>
   )
 }
