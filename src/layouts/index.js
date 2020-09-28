cab892c8e830fa616b905422ef43c78845d27871
Celia Gomez <celia.gomez@unumotors.com>
2020-09-28 14:10:47 +0200

    feat: install smooth scrolling library

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,6 +5,11 @@ import "../assets/style/main.scss"
 import { Store } from "../context"
 
 export default function Layout({ children }) {
+  if (typeof window !== "undefined") {
+    // eslint-disable-next-line global-require
+    require("smooth-scroll")('a[href*="#"]')
+  }
+
   return (
     <div id="layout">
       <Store>
