dd61b5169436668bdb38881cbaa23eeb6fe8554c
Celia Gomez <celia.gomez@unumotors.com>
2020-10-01 16:24:57 +0200

    feat: fix responsive issues

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,50 +4,48 @@ import AkiraAnimation from "./akira-animation"
 
 export default function Contact() {
   return (
-    <section id="contact" >
-      <div className={`grid content`}>
-        <div className={`main-section-copy ${styles.sectionCopy}`}>
-          <h1>Get in touch</h1>
-          <p>
-            Drop me a line if you have any questions or you would like to work
-            on some project together. It would be great to hear about you!{" "}
-            <a href="mailto:celiagomezdev@gmail.com">celiagomezdev@gmail.com</a>
-            .
-            <br />
-            <br />
-            You can also check my{" "}
-            <a
-              href="https://github.com/celiagomezdev"
-              target="_blank"
-              rel="noreferrer"
-            >
-              github
-            </a>
-            ,{" "}
-            <a
-              href="https://www.linkedin.com/in/celiagomezdev/?locale=en_US"
-              target="_blank"
-              rel="noreferrer"
-            >
-              linkedin
-            </a>{" "}
-            or{" "}
-            <a
-              href="https://www.instagram.com/celiagomezdev"
-              target="_blank"
-              rel="noreferrer"
-            >
-              instagram
-            </a>{" "}
-            (personal analog pictures) to know a little bit more about me.
-            <br />
-            <br />
-            See you soon! 🙋🏻‍♀️
-          </p>
-        </div>
-        <div className={styles.imageSection}>
-          <AkiraAnimation />
-        </div>
+    <section id={styles.contact} className={`content grid`}>
+      <div className={`main-section-copy ${styles.sectionCopy}`}>
+        <h1>Get in touch</h1>
+        <p>
+          Drop me a line if you have any questions or you would like to work
+          on some project together. It would be great to hear about you!{" "}
+          <a href="mailto:celiagomezdev@gmail.com">celiagomezdev@gmail.com</a>
+          .
+          <br />
+          <br />
+          You can also check my{" "}
+          <a
+            href="https://github.com/celiagomezdev"
+            target="_blank"
+            rel="noreferrer"
+          >
+            github
+          </a>
+          ,{" "}
+          <a
+            href="https://www.linkedin.com/in/celiagomezdev/?locale=en_US"
+            target="_blank"
+            rel="noreferrer"
+          >
+            linkedin
+          </a>{" "}
+          or{" "}
+          <a
+            href="https://www.instagram.com/celiagomezdev"
+            target="_blank"
+            rel="noreferrer"
+          >
+            instagram
+          </a>{" "}
+          (personal analog pictures) to know a little bit more about me.
+          <br />
+          <br />
+          See you soon! 🙋🏻‍♀️
+        </p>
+      </div>
+      <div className={styles.imageSection}>
+        <AkiraAnimation />
       </div>
     </section>
   )
