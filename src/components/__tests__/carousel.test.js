0d9f5baea31198472583e0f8359a94c8dbe5fa24
Celia Gomez <celia.gomez@unumotors.com>
2020-10-29 10:30:03 +0100

    test(carousel): fix issue with computed styles not rendering

--- src/components/__tests__/carousel.test.js
+++ src/components/__tests__/carousel.test.js
@@ -1,26 +1,43 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
 import { Context } from "../../context"
+import fs from 'fs'
+import path from 'path'
 import Carousel from "../carousel"
 
 let state
+// Mock dispatch method
 const dispatch = jest.fn()
 
+// Inject computed styles into jsdom
+const injectCSS = (container) => {
+  const cssFile = fs.readFileSync(
+    path.resolve(__dirname, '../carousel.module.scss'),
+    'utf8'
+  )
+  const style = document.createElement('style')
+  style.innerHTML = cssFile
+  container.append(style)
+}
+
 beforeEach(() => {
+  // Mock state
   state = { activeSlide: 0 }
 
-  // render Carousel Component before each test
-  render(
+  // Render Carousel Component before each test
+  const { container } = render(
     <Context.Provider value={[state, dispatch]}>
       <Carousel />
     </Context.Provider>
   )
+
+  injectCSS(container)
 })
 
 describe("Carousel", () => {
   it("has the left arrow button hidden on first slide (initial state)", () => {
     const leftArrowButton = screen.getByLabelText("Go to previous slide")
-    expect(leftArrowButton).toHaveClass('hidden')
+    expect(leftArrowButton).not.toBeVisible()
   })
 })
 
