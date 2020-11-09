6a3045c72d47a3210a66297d968ce9f6035fc608
Celia Gomez <celia.gomez@unumotors.com>
2020-11-09 14:25:06 +0100

    feat: implement mobile state logics

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,21 @@
-import React from "react"
+import React, { useEffect, useContext } from "react"
 import Hero from "../components/hero"
 import CeliaAnimation from "../components/celia-animation"
 import MiddleSection from "../components/middle-section"
 import Contact from "../components/contact"
+import { Context } from "../context"
+import { ACTION_TYPES } from "../constants"
 
 export default function HomePage() {
+  const windowGlobal = typeof window !== 'undefined' && window
+  // eslint-disable-next-line no-unused-vars
+  const [state, dispatch] = useContext(Context)
+
+  useEffect(() => {
+    if (windowGlobal.innerWidth <= 767) dispatch({ type: ACTION_TYPES.SET_IS_MOBILE, isMobile: true})
+    else dispatch({ type: ACTION_TYPES.SET_IS_MOBILE, isMobile: false})
+  }, [windowGlobal.innerWidth, dispatch])
+
   return (
     <main id="home-page">
       <Hero />     
